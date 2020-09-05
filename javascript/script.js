1bea18b008a813a860b14e503c51e79d991c043b
Stephanie Cruz <scruz.17@est.ucab.edu.ve>
2020-09-05 13:35:51 -0400

    Agregar animaciones con waypoint

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,53 @@
 
+  $(document).ready(function() {
+        $('.logos-container').waypoint(function(direction){
+          $('.logos-container').addClass('animated fadeInLeft'); 
+      },{offset:'100%;'})
+        $('.clients-quote-container').waypoint(function(direction){
+          $('.clients-quote-container').addClass('animated fadeInRight'); 
+      },{offset:'100%;'})
+        $('.us-features').waypoint(function(direction){
+          $('.us-features').addClass('animated fadeInRight'); 
+      },{offset:'100%;'})
+      $('.js-animated-right').waypoint(function(direction){
+          $('.js-animated-right').addClass('animated fadeInRight'); 
+    },{offset:'100%;'})
+    $('.js-animated-left').waypoint(function(direction){
+         $('.js-animated-left').addClass('animated fadeInLeft'); 
+    },{offset:'100%;'})
+    $('.us-quote__card').waypoint(function(direction){
+        $('.us-quote__card').addClass('animated fadeInLeft'); 
+    },{offset:'100%;'})
+    $('.us-quote__card').waypoint(function(direction){
+        $('.us-quote__card').addClass('animated fadeInLeft'); 
+    },{offset:'100%;'})
+    $('.team-card').waypoint(function(direction){
+        $('.team-card').addClass('animated bounceIn'); 
+    },{offset:'100%;'})
+    $('.team-card').waypoint(function(direction){
+        $('.team-card').addClass('animated bounceIn'); 
+  },{offset:'100%;'})
+    $('.blog-card').waypoint(function(direction){
+        $('.blog-card').addClass('animated bounceIn'); 
+    },{offset:'100%;'})
+    $('.blog-post__article__image').waypoint(function(direction){
+        $('.blog-post__article__image').addClass('animated fadeInLeft'); 
+    },{offset:'100%;'})   
+    $('.service__post__cite').waypoint(function(direction){
+        $('.service__post__cite').addClass('animated fadeInLeft'); 
+    },{offset:'100%;'})
+    $('.service__side').waypoint(function(direction){
+        $('.service__side').addClass('animated fadeInRight'); 
+    },{offset:'100%;'})
+    $('.question-cards-container').waypoint(function(direction){
+      $('.question-cards-container').addClass('animated fadeInLeft'); 
+  },{offset:'100%;'})
+    $('.team-qualities__sections').waypoint(function(direction){
+      $('.progress-bar__content__top__percentage').addClass('animated animatedPercentage'); 
+    },{offset:'100%;'})
+
+    
+});
   // carousel
   let slider_index = 0;
 
@@ -86,6 +135,7 @@ var typed = new Typed('#typed', {
       }
     }
   });
+
 //animations
     $('.header__title').waypoint(function(direction){
         $('.header__title').addClass('animated fadeInLeft');  
@@ -108,7 +158,6 @@ var typed = new Typed('#typed', {
     },{offset:'100%;'})
 
     $('.social-box').waypoint(function(direction){
-        $('.social-box').addClass('animated fadeInLeft');  
+        $('.social-box').addClass('animated fadeInLeft'); 
     },{offset:'100%;'})
  
-
