d6bfe83ddbd459f8365a8688404860378e24f9ae
Stephanie Cruz <scruz.17@est.ucab.edu.ve>
2020-10-03 09:07:24 -0400

    Agregar seccion de tecnicas panaderas

---
+++ _culinary-blog-techniques/masa-madre.md
@@ -0,0 +1,28 @@
+---
+layout: culinary-blog-template/culinary-blog-techniques
+identifier: techn3
+page-image: technique-1.png #Imagen para redes sociales
+title: LA MASA MADRE | Tecnicas
+video: amasado.mp4
+subtitle: Lorem ipsum dolor sit amen
+basic-info:
+  name: La masa madre biológica
+  primary-image:
+    name: technique-1.jpg
+    alt: Masa madre
+  url: 'techniques/masa-madre.html'
+information:
+  - name: 'El amasado tiene dos finalidades:'
+    type: paragraph
+  - name: 'Mezclar de forma homogénea: agua, harina, sal, levadura y   eventualmente mejoradores.'
+    type: check
+  - check:
+  - title:
+  - paragraph:
+  - subtitle:
+  - parapraph:
+  - check:
+  - check:
+  - check:
+  - check:  
+---
---
+++ javascript/pagination.js
@@ -0,0 +1,83 @@
+(function(){
+
+    pages = document.querySelectorAll("#page-btn");
+    elements = document.getElementsByClassName('card-cont');
+    max_results = elements.length;
+	no_of_pages = Math.ceil( max_results / config.max_per_page );
+    
+    function init(){ 
+        update_page();
+        document.getElementById("next").onclick = function() { 
+			pager("next");
+			return false;
+		};
+		document.getElementById("prev").onclick = function() { 
+			pager("prev"); 
+			return false;
+		};
+        for(var i = 0; i<pages.length; i++) {
+            pages[i].onclick = function(e) { 
+                var page = e.srcElement.getAttribute("page");
+                if(page){
+                    pager("goto", page);
+                }
+                return false;
+            }; 
+        }        
+    }
+
+    function pager(action, page) {
+       
+		switch (action) {
+            case "next":
+				if( (config.page + 1) <= no_of_pages ){ 
+					++config.page;
+				}
+				break; 
+			case "prev":
+				if( (config.page - 1) >= 1 ){
+					 --config.page;
+				}
+				break;
+			case "goto":
+				config.page = page;
+				break;
+			default:
+				break;
+		}
+		update_page();
+	}
+
+    function update_page(){
+		if (config.scroll == true){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+        }    
+            $('.card-cont').css('transform', 'scale(0)');
+            $('.card-cont').css('display', 'none');
+
+        let finish = config.page*config.max_per_page;
+        let start = finish - config.max_per_page;
+        finish= finish -1;
+        
+        for(var i = start; i<=finish; i++) {
+           elements[i].style.transform='scale(1)';
+           elements[i].style.display='block';         
+        }
+	}
+
+
+    for(var i = 0; i<pages.length; i++) {
+        pages[i].onclick = function(e) { 
+            var page = e.srcElement.getAttribute("page");
+            if(page){
+                pager("goto", page);
+            }
+            return false;
+        }; 
+    }      
+
+	window.addEventListener("load", function() {
+        init();     
+	});
+})();
+
